2d727e5b3254268375c9d8bdb7486deb353bf747
Yifan Gao <git@yfgao.com>
2017-11-04 16:47:44 +0800

    telegraf connection stat: fix default value

--- telegraf/connection.sh
+++ telegraf/connection.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 port_list_input=${1//:/|}
-port_list=${port_list_input:-"80:443"}
+port_list=${port_list_input:-"80|443"}
 for type in CLOSE_WAIT CLOSING ESTABLISHED FIN_WAIT1 FIN_WAIT2 LAST_ACK SYN_RECV TIME_WAIT; do
-	netstat -ntW  | grep $type | awk '{print $4}' | grep -P ":($port_list)\$" | sort | uniq -c | sort -k 2 | 
+	netstat -ntW  | grep $type | awk '{print $4}' | grep -P ":($port_list)\$" | sort | uniq -c | sort -k 2 |
 	while read line; do
 		count=$(awk '{print $1}' <<< $line)
 		socket=$(awk '{print $2}' <<< $line)
